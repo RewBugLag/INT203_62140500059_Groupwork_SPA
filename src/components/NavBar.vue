49762e36d4275f578a3555fbc4d362dfd5ca12f1
RewBugLag <rewrew2000@gmail.com>
2021-04-16 20:47:18 +0700

    [62130500059] Added NavBar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <nav class="bg-gray-700">
+        <ul class="flex flex-row justify-end">
+            <li><router-link to="/">Register Employee</router-link></li>
+            <li><router-link to="/EmployeeList">Employee List</router-link></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'NavBar'
+}
+</script>
+
+<style scoped>
+    nav ul li {
+        color: white;
+        padding: 15px 30px;
+    }
+    nav ul li:hover {
+        background-color: rgb(5, 150, 105);
+    }
+</style>

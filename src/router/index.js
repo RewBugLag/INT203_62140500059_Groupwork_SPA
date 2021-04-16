9933cbc3f4ca6ff02aaa213b9048f27f1d3b130e
RewBugLag <rewrew2000@gmail.com>
2021-04-16 17:52:08 +0700

    [62130500059] Deleted default unused components (Home, About)

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import EmployeeRegister from '../views/EmployeeRegister.vue'
+import EmployeeList from '../views/EmployeeList.vue'
+
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'EmployeeRegister',
+    component: EmployeeRegister
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/EmployeeList',
+    name: 'EmployeeList',
+    component: EmployeeList
   }
 ]
 

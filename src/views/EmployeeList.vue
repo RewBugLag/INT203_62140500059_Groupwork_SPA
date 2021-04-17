2d8b6af0378fffc7c07f1543ae89db72d5641dd4
RewBugLag <rewrew2000@gmail.com>
2021-04-17 13:14:55 +0700

    [62130500059] Added PUT method for Editing Employee

--- src/views/EmployeeList.vue
+++ src/views/EmployeeList.vue
@@ -12,7 +12,7 @@
                     <td>{{employee.email}}</td>
                     <td>
                         <button class="inline-block px-8 py-2 mr-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2"
-                            @click="editEmployee(employee.id)">
+                            @click="editModeOn(employee.id, employee.name, employee.email)">
                             Edit
                         </button>
                         <button class="inline-block px-8 py-2 text-white bg-red-500 rounded-lg hover:bg-red-600 focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2"
@@ -23,28 +23,48 @@
                 </tr>
             </table>
         </div>
+        <div v-if="editMode" class="px-4 py-8 mt-8 border border-gray-50 rounded-xl shadow bg-white">
+            <BaseForm topic="Employee Edit Form" @submit-form="editEmployee">
+                <BaseInput inputId="name" @submit-input="submitInput" />
+                <BaseInput inputId="email" @submit-input="submitInput" />
+            </BaseForm>
+        </div>
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
-// import BaseForm from '@/components/BaseForm.vue'
-// import BaseInput from '@/components/BaseInput.vue'
+import BaseForm from '@/components/BaseForm.vue'
+import BaseInput from '@/components/BaseInput.vue'
 
 export default {
     name: 'EmployeeRegister',
     components: {
-        // BaseForm,
-        // BaseInput
+        BaseInput,
+        BaseForm
     },
     data() {
         return {
             url: 'http://localhost:5000/employeeList',
-            employeeList: []
+            employeeList: [],
+            editMode: false,
+            editingEmployee: {
+                id: Number,
+                name: String,
+                email: String
+            }
         }
     },
-    // Async-Await form
     methods: {
+        editModeOn(employeeId, employeeName, employeeEmail) {
+            this.editMode = true
+            this.editingEmployee['id'] = employeeId
+            this.editingEmployee['name'] = employeeName
+            this.editingEmployee['email'] = employeeEmail
+        },
+        submitInput(inputId, enteredInput) {
+            this.editingEmployee[inputId] = enteredInput
+        },
 		async fetchEmployeeList() {
 			try {
 				const res = await fetch(this.url)
@@ -54,26 +74,27 @@ export default {
 				console.log(error)
 			}
 		},
-        async editEmployee(employeeId) {
-            console.log(employeeId)
-            // const res = await fetch(`${this.url}/${employeeId}`, {
-			// 	method: 'PUT',
-			// 	headers: {
-			// 		'Content-type': 'application/json'
-			// 	},
-			// 	body: JSON.strongify({
-			// 		name: employee.name,
-			// 		email: employee.email
-			// 	})
-            // })
-            // const data = await res.json()
-            // this.employeeList = this.employeeList.map((employee) =>
-			// 	employee.id === data.id ? {
-			// 		...employee,
-			// 		name: data.name,
-			// 		email: data.email
-			// 	} : employee
-            // )
+        async editEmployee() {
+            const res = await fetch(`${this.url}/${this.editingEmployee.id}`, {
+				method: 'PUT',
+				headers: {
+					'Content-type': 'application/json'
+				},
+				body: JSON.stringify({
+					name: this.editingEmployee.name,
+					email: this.editingEmployee.email
+				})
+            })
+            const data = await res.json()
+            this.employeeList = this.employeeList.map((employee) =>
+				employee.id === data.id ? {
+					...employee,
+					name: data.name,
+					email: data.email
+				} : employee
+            )
+
+            this.editMode = false
         },
         async deleteEmployee(employeeId) {
             const res = await fetch(`${this.url}/${employeeId}`, {
